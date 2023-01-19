4f1e989cd41c121db32471f9076ccc77b7adcb8e
PrimeFord <bashfordbash@gmail.com>
2023-01-19 21:23:00 +0100

    Figma Landing Page Clone

---
+++ src/App.jsx
@@ -0,0 +1,339 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="nav-bar">
+        <p>Logo</p>
+        <div className="links">
+          <a href="http://">Button 1</a>
+          <a href="http://">Button 2</a>
+          <a href="http://">Button 3</a>
+          <a href="http://">Button 4</a>
+        </div>
+        <div className="btn">
+          <button>Button 1</button>
+          <button>Button 2</button>
+        </div>
+      </nav>
+      <div className="wrp1">
+        <p className="title">Welcome to the best website template!</p>
+        <p className="para">
+          Are you looking for solutions to your problems?Look futher, because we
+          have the answer. Our team of expert has the skills and experience to
+          deliver top-quality results.
+        </p>
+        <div className="opt">
+          <button>Button 1</button>
+          <button>Button 2</button>
+        </div>
+        <div className="pics">
+          <img src="./image/pic1.png" alt="pic" />
+          <img src="./image/pic2.jpg" alt="pic" />
+        </div>
+      </div>
+      <div className="wrp2">
+        <div className="line">
+          <div className="one">
+            <div className="head">
+              <span>
+                <img src="./image/Icon1.png" alt="icon" />
+              </span>
+              Header
+            </div>
+            <p className="word">
+              Ant Design, a design language for background applications, is
+              refined by Ant UED Team. Ant Design, a design language for
+              background applications, is refined by Ant UED Team.Ant Design, a
+              design language for background applications, is refined by Ant UED
+              Team.Ant Design, a design language for background...
+            </p>
+            <div className="btn_arrow">
+              <a href="http://">Button</a>
+              <span>
+                <img src="./image/arrow.png" alt="arrow" />
+              </span>
+            </div>
+          </div>
+          <div className="one">
+            <div className="head">
+              <span>
+                <img src="./image/Icon2.png" alt="icon" />
+              </span>
+              Header
+            </div>
+            <p className="word">
+              Ant Design, a design language for background applications, is
+              refined by Ant UED Team. Ant Design, a design language for
+              background applications, is refined by Ant UED Team.Ant Design, a
+              design language for background applications, is refined by Ant UED
+              Team.Ant Design, a design language for background...
+            </p>
+            <div className="btn_arrow">
+              <a href="http://">Button</a>
+              <span>
+                <img src="./image/arrow.png" alt="arrow" />
+              </span>
+            </div>
+          </div>
+          <div className="one">
+            <div className="head">
+              <span>
+                <img src="./image/Icon3.png" alt="icon" />
+              </span>
+              Header
+            </div>
+            <p className="word">
+              Ant Design, a design language for background applications, is
+              refined by Ant UED Team. Ant Design, a design language for
+              background applications, is refined by Ant UED Team.Ant Design, a
+              design language for background applications, is refined by Ant UED
+              Team.Ant Design, a design language for background...
+            </p>
+            <div className="btn_arrow">
+              <a href="http://">Button</a>
+              <span>
+                <img src="./image/arrow.png" alt="arrow" />
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="wrp3">
+        <div className="card">
+          <div className="details">
+            <h2>Why you should choose us</h2>
+            <p>
+              Ant Design, a design language for background applications, is
+              refined by Ant UED Team. Ant Design, a design language for
+              background applications, is refined by Ant UED Team.Ant Design, a
+              design language for background applications, is refined by Ant UED
+              Team.Ant Design, a design language for
+            </p>
+            <p>
+              Ant Design, a design language for background applications, is
+              refined by Ant UED Team. Ant Design, a design language for
+              background applications, is refined by Ant UED Team.Ant Design, a
+              design language for background applications, is refined by Ant UED
+              Team.Ant Design, a design language for
+            </p>
+          </div>
+          <div className="d_img">
+            <img src="./image/art1.png" alt="" />
+          </div>
+          <div className="d_img">
+            <img src="./image/art2.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="wrp4">
+        <div className="card">
+          <div className="bird_img">
+            <h2>Why you should us</h2>
+            <img src="./image/bird.png" alt="" />
+          </div>
+          <div className="sub">
+            <h2>Subheader</h2>
+            <p>
+              Ant Design, a design language for background applications, is
+              refined by Ant UED Team. Ant Design, a design language for
+              background applications, is refined by Ant UED Team.Ant Design, a
+              design language for background applications, is refined by Ant UED
+              Team.Ant Design, a design language for
+            </p>
+            <p>
+              Ant Design, a design language for background applications, is
+              refined by Ant UED Team. Ant Design, a design language for
+              background applications, is refined by Ant UED Team.Ant Design, a
+              design language for background applications, is refined by Ant UED
+              Team.Ant Design, a design language for
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="wrp5">
+        <h2>Gallery</h2>
+        <div className="cards">
+          <img className="gal gal1" src="./image/gal1.png" alt="gallery1" />
+          <img className="gal" src="./image/gal2.png" alt="gallery2" />
+          <img className="gal" src="./image/gal3.png" alt="gallery3" />
+          <img className="gal" src="./image/gal4.png" alt="gallery4" />
+          <img className="gal" src="./image/gal5.png" alt="gallery5" />
+        </div>
+      </div>
+      <div className="wrp6">
+        <h2>Meet the Team</h2>
+
+        <div className="card1">
+          <p>
+            Ant Design, a design language for background applications, is
+            refined by Ant UED Team. Ant Design, a design language for
+            background applications, is refined by Ant UED Team.Ant Design, a
+            design language for background applications, is refined by Ant UED
+            Team.Ant Design, a design language for background applications, is
+            refined by Ant UED Team.Ant Design, a design language for background
+          </p>
+          <div className="box">
+            <div className="dir">
+              <img src="./image/arr1.png" alt="left-arrow" />
+              <img src="./image/arr2.png" alt="right-arrow" />
+            </div>
+            <div className="profile">
+              <div className="pple1">
+                <img src="./image/gal3.png" alt="" />
+                <h4>Header</h4>
+                <p>Caption</p>
+              </div>
+              <div className="pple1">
+                <img src="./image/gal3.png" alt="" />
+                <h4>Header</h4>
+                <p>Caption</p>
+              </div>
+              <div className="pple1">
+                <img src="./image/gal3.png" alt="" />
+                <h4>Header</h4>
+                <p>Caption</p>
+              </div>
+              <div className="pple1">
+                <img src="./image/gal3.png" alt="" />
+                <h4>Header</h4>
+                <p>Caption</p>
+              </div>
+              <div className="pple1">
+                <img src="./image/gal3.png" alt="" />
+                <h4>Header</h4>
+                <p>Caption</p>
+              </div>
+              <div className="pple1">
+                <img src="./image/gal3.png" alt="" />
+                <h4>Header</h4>
+                <p>Caption</p>
+              </div>
+              <div className="pple1">
+                <img src="./image/gal3.png" alt="" />
+                <h4>Header</h4>
+                <p>Caption</p>
+              </div>
+              <div className="pple1">
+                <img src="./image/gal3.png" alt="" />
+                <h4>Header</h4>
+                <p>Caption</p>
+              </div>
+              <div className="pple1">
+                <img src="./image/gal3.png" alt="" />
+                <h4>Header</h4>
+                <p>Caption</p>
+              </div>
+              <div className="pple1">
+                <img src="./image/gal3.png" alt="" />
+                <h4>Header</h4>
+                <p>Caption</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="wrp7">
+        <div className="phone">
+          <img src="./image/phone.png" alt="phone" />
+        </div>
+        <div className="form">
+          <h2>Contact us</h2>
+          <p>Our lovely team would like to hear from you</p>
+          <div className="forms">
+            <section>
+              <label htmlFor="Firstname">First name</label>
+              <input
+                type="text"
+                name="firstname"
+                id="first_name"
+                placeholder="example"
+              />
+            </section>
+            <section>
+              <label htmlFor="Lastname">Last name</label>
+              <input
+                type="text"
+                name="lastname"
+                id="last_name"
+                placeholder="example"
+              />
+            </section>
+            <section>
+              <label htmlFor="Firstname">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="example"
+              />
+            </section>
+            <section>
+              <label htmlFor="phone_no">Phone number</label>
+              <input
+                type="text"
+                name="phone no"
+                id="phone_no"
+                placeholder="example"
+              />
+            </section>
+            <section className="text">
+              <label htmlFor="issue">Issue</label>
+              <textarea
+                name="issue"
+                id="issue"
+                cols="10"
+                rows="5"
+                placeholder="example"
+              ></textarea>
+            </section>
+            <section className="submit">
+              <button type="submit">
+                Submit <img src="./image/Vector.png" alt="" />
+              </button>
+            </section>
+          </div>
+          <div className="social">
+            <img src="./image/github.png" alt="" />
+            <img src="./image/mail.png" alt="" />
+            <img src="./image/twitter.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="wrp8">
+        <div className="foot">
+          <div className="summary">
+            <h3>Logo</h3>
+            <p>
+              Ant Design, a design language for background applications, is
+              refined by Ant UED Team. Ant Design, a design language for
+              background applications, is refined by Ant UED Team.Ant Design, a
+              design
+            </p>
+          </div>
+          <div className="contact">
+            <h3>Contact us</h3>
+            <p>
+              <a href="http://">Button</a>
+              <a href="http://">Button</a>
+              <a href="http://">Button</a>
+            </p>
+          </div>
+          <div className="subscribe">
+            <h3>Logo</h3>
+            <p>
+              Ant Design, a design language for background applications, is
+              refined by Ant UED Team. Ant Design
+            </p>
+            <main className="subform">
+              <input type="text" placeholder="example" />
+              <button type="submit">Button</button>
+            </main>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
